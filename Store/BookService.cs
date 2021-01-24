a026ff8e0e2dc872702b6e5096ffec12288f1ec5
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-24 22:38:05 +0300

    Добавить класс BookService

---
+++ Store/BookService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    public class BookService
+    {
+        IEnumerable<Book> GetAllByQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

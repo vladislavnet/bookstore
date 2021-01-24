0822b3a11fea62d0fa2b7eb37bdca048431a1820
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-25 00:20:26 +0300

    Реализовать тесты на проверку метода GetAllByQuery

--- domain/Store.Tests/BookServiceTests.cs
+++ domain/Store.Tests/BookServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -10,7 +11,31 @@ namespace Store.Tests
         [Fact]
         public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
         {
+            var bookRepositoryMoq = new Mock<IBookRepository>();
+            bookRepositoryMoq.Setup(x => x.GetByAllIsbn(It.IsAny<string>()))
+                             .Returns(new[] { new Book(1, "", 1, new Author(1, ""), "") });
 
+            bookRepositoryMoq.Setup(x => x.GetAllByTitleOrAutror(It.IsAny<string>()))
+                             .Returns(new[] { new Book(2, "", 2, new Author(2, ""), "") });
+
+            var bookService = new BookService(bookRepositoryMoq.Object);
+
+            Assert.Collection(bookService.GetAllByQuery("ISBN 12345-67890"), book => Assert.Equal(1, book.Id));
+        }
+
+        [Fact]
+        public void GetAllByQuery_WithAuthor_CallsGetAllByTitleOrAuthor()
+        {
+            var bookRepositoryMoq = new Mock<IBookRepository>();
+            bookRepositoryMoq.Setup(x => x.GetByAllIsbn(It.IsAny<string>()))
+                             .Returns(new[] { new Book(1, "", 1, new Author(1, ""), "") });
+
+            bookRepositoryMoq.Setup(x => x.GetAllByTitleOrAutror(It.IsAny<string>()))
+                             .Returns(new[] { new Book(2, "", 2, new Author(2, ""), "") });
+
+            var bookService = new BookService(bookRepositoryMoq.Object);
+
+            Assert.Collection(bookService.GetAllByQuery("Martin Fauler"), book => Assert.Equal(2, book.Id));
         }
     }
 }

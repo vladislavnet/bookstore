bc761f45cde36a2b989da89fd623f9f3721478eb
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-24 17:45:42 +0300

    Добавить реализацию реаозитория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly IEnumerable<Book> books = new Book[] 
+        { 
+            new Book(1, "Refactoring"),
+            new Book(2, "Pro Angular 9"),
+            new Book(3, "Microsoft Blazor"),
+        };
+        public IEnumerable<Book> GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart));
+        }
+    }
+}

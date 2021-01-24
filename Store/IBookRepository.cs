966dbdf961305e4c744cf2a7891aea57f30d1f84
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-24 17:28:16 +0300

    Добавить интерфейс репозитория книг

---
+++ Store/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetAllByTitle(string titlePart);
+    }
+}

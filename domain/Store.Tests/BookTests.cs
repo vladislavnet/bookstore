e13b5de2ccfde9700c78bd462ed6f8b92473078e
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-24 23:46:51 +0300

    Добавить тесты на проверку метода IsIsbn

--- domain/Store.Tests/BookTests.cs
+++ domain/Store.Tests/BookTests.cs
@@ -6,9 +6,27 @@ namespace Store.Tests
     public class BookTests
     {
         [Fact]
-        public void Test1()
+        public void IsIsbn_WithNull_ReturnFalse()
         {
+            Assert.False(Book.IsIsbn(null));
+        }
 
+        [Fact]
+        public void IsIsbn_WithBlankString_ReturnFalse()
+        {
+            Assert.False(Book.IsIsbn("    "));
+        }
+
+        [Fact]
+        public void IsIsbn_WithInvalidIsbn_ReturnFalse()
+        {
+            Assert.False(Book.IsIsbn("ISBN 123"));
+        }
+
+        [Fact]
+        public void IsIsbn_WithIsbn10_ReturnTrue()
+        {
+            Assert.False(Book.IsIsbn("IsBN 123-123-123 0"));
         }
     }
 }

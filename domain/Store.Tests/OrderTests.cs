44ae52d4ffbb40d8d981565dc52307ae2de7ae9a
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-25 17:39:02 +0300

    Добавить тесты на проверку логики класса Order

---
+++ domain/Store.Tests/OrderTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Store.Tests
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void Order_WithNullItems_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+            {
+                new Order(1, null);
+            });
+        }
+
+        [Fact]
+        public void TotalCount_WithEmptyItems_ReturnsZero()
+        {
+            var order = new Order(1, new List<OrderItem>());
+            Assert.Equal(0, order.TotalCount);
+        }
+
+        [Fact]
+        public void TotalPrice_WithEmptyItems_ReturnsZero()
+        {
+            var order = new Order(1, new List<OrderItem>());
+            Assert.Equal(0m, order.TotalPrice);
+        }
+
+        [Fact]
+        public void TotalCount_WithNonEmptyItems_CalculatesTotalCount()
+        {
+            var order = new Order(1, new List<OrderItem>() 
+            { 
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m),
+            });
+            Assert.Equal(3 + 5, order.TotalCount);
+        }
+
+        [Fact]
+        public void TotalPrice_WithNonEmptyItems_CalculatesTotalPrice()
+        {
+            var order = new Order(1, new List<OrderItem>()
+            {
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m),
+            });
+            Assert.Equal(3 * 10m + 5 * 100m, order.TotalPrice);
+        }
+    }
+}

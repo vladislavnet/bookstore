46ab75ef04a426bf10e7649fbfcf0e2c39a033de
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-25 22:48:25 +0300

    Изменить класс Cart

--- presentation/Store.Web/Models/Cart.cs
+++ presentation/Store.Web/Models/Cart.cs
@@ -7,8 +7,15 @@ namespace Store.Web.Models
 {
     public class Cart
     {
-        public IDictionary<int, int> Items { get; set; } = new Dictionary<int, int>();
+        public int OrderId { get; }
+        public int TotalCount { get; set; }
+        public decimal TotalPrice { get; set; }
 
-        public decimal Amount { get; set; }
+        public Cart(int orderId)
+        {
+            OrderId = orderId;
+            TotalCount = 0;
+            TotalPrice = 0m;
+        }
     }
 }

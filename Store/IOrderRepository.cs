352e5b28d4ca4bb56c15e5d5fdc22b6e71f233e3
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-25 17:42:07 +0300

    Создать интерфейс репозиторий заказа

---
+++ Store/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    public interface IOrderRepository
+    {
+        Order Create();
+        Order GetById(int id);
+        void Update(Order order);
+    }
+}

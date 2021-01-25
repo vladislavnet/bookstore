3795a2edfc83e763b82f80e7b66d3d2b08251f6b
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-25 17:26:09 +0300

    Создать класс Order

---
+++ Store/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store
+{
+    public class Order
+    {
+        public int Id { get; }
+
+        private List<OrderItem> items;
+
+        public IReadOnlyCollection<OrderItem> Items => items;
+
+        public int TotalCount => items.Sum(item => item.Count);
+        public decimal TotalPrice => items.Sum(item => item.Price * item.Count);
+
+        public Order(int id, IEnumerable<OrderItem> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+            Id = id;
+            this.items = new List<OrderItem>(items);
+        }
+
+    }
+}

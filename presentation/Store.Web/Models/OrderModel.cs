9a2a03bdf3cff0026f0c7b2138c57ef872ab8613
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-26 23:13:34 +0300

    Revert "Добавить словарь ошибок в  OrderModel"
    This reverts commit 82ccb7f4020784772ee064fb72346a3c2a12214c.

--- presentation/Store.Web/Models/OrderModel.cs
+++ presentation/Store.Web/Models/OrderModel.cs
@@ -11,6 +11,5 @@ namespace Store.Web.Models
         public OrderItemModel[] Items { get; set; } = new OrderItemModel[0];
         public int TotalCount { get; set; }
         public decimal TotalPrice { get; set; }
-        public Dictionary<string, string> Errors { get; set; } = new Dictionary<string, string>();
     }
 }

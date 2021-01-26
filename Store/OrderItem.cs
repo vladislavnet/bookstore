50809b4224afc8202fe04bf24cbc1a5016cadbc4
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-26 17:27:04 +0300

    Сделать свойство Count класса OrderItem мутабельным

--- Store/OrderItem.cs
+++ Store/OrderItem.cs
@@ -6,17 +6,31 @@ namespace Store
 {
     public class OrderItem
     {
+        private int count;
         public int BookId { get; }
-        public int Count { get; }
+        public int Count 
+        {
+            get => count;
+            set
+            {
+                ThrowIfInvalidCount(value);
+                count = value;
+            }
+        }
         public decimal Price { get; }
 
         public OrderItem(int bookId, int count, decimal price)
         {
-            if (count <= 0)
-                throw new ArgumentOutOfRangeException("Count must be greater than zero");
+            ThrowIfInvalidCount(count);
             BookId = bookId;
             Count = count;
             Price = price;
+        }
+
+        private static void ThrowIfInvalidCount(int count)
+        {
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException("Count must be greater than zero");
         }
     }
 }

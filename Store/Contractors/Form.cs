7fb56e5cc7bdaafdcb92edc4c494b07eace0e7a5
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-27 15:05:28 +0300

    Добавить класс Form и Field

---
+++ Store/Contractors/Form.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Contractors
+{
+    public class Form
+    {
+        public string UniqueCode { get; }
+        public int OrderId { get; }
+        public int Step { get; }
+        public bool IsFinal { get; }
+        public IReadOnlyList<Field> Fields { get; }
+
+        public Form(string code, int orderId, int step, bool isFinal, IEnumerable<Field> fields)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+                throw new ArgumentException(nameof(code));
+
+            if (step < 1)
+                throw new ArgumentOutOfRangeException(nameof(step));
+
+            if (fields == null)
+                throw new ArgumentNullException(nameof(fields));
+
+            UniqueCode = code;
+            OrderId = orderId;
+            Step = step;
+            IsFinal = isFinal;
+            Fields = fields;
+        }
+    }
+}

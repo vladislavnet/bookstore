54edb071eea836641118e18706fa404b404cd5ba
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-27 15:22:51 +0300

    Добавить класс DeliveryModel

---
+++ presentation/Store.Web/Models/DeliveryModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Store.Web.Models
+{
+    public class DeliveryModel
+    {
+        public int OrderId { get; set; }
+        public Dictionary<string, string> Methods { get; set; }
+    }
+}

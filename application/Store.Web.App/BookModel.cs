1629c999985d48b653220fc3349fb535c0a656ca
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-01-31 13:09:59 +0300

    Добавить BookModel

---
+++ application/Store.Web.App/BookModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Store.Web.App
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+
+        public string Isbn { get; set; }
+        public int AuthorId { get; set; }
+
+        public string AuthorFullname { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}

c7417dd02b883a42831d7ce0c08df259343b0af4
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-02-02 22:58:54 +0300

    Отправлять уведомление о завершении заказа

--- Store/Messages/INotificationService.cs
+++ Store/Messages/INotificationService.cs
@@ -7,5 +7,6 @@ namespace Store.Messages
     public interface INotificationService
     {
         void SendConfirmationCode(string cellPhone, int code);
+        void StartProcess(Order order);
     }
 }

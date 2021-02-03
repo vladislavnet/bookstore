16095c9e472bf9b353e7df388852900de6e20fac
vladislavnet <vladislaus.panyukov@yandex.ru>
2021-02-03 09:13:12 +0300

    Подредактировать метод отправки mail

--- Store/Messages/DebugNotificationService.cs
+++ Store/Messages/DebugNotificationService.cs
@@ -13,21 +13,25 @@ namespace Store.Messages
 
         public void StartProcess(Order order)
         {
-            using (var client = new SmtpClient())
-            {
-                var message = new MailMessage("from@at.my.domain", "to@at.my.domain");
-                message.Subject = "Заказ #" + order.Id;
+            //using (var client = new SmtpClient())
+            //{
+            //    var message = new MailMessage("from@at.my.domain", "to@at.my.domain");
+            //    message.Subject = "Заказ #" + order.Id;
 
-                var builder = new StringBuilder();
-                foreach (var item in order.Items)
-                {
-                    builder.Append("{0}, {1}", item.BookId, item.Count);
-                    builder.AppendLine();
-                }
+            //    var builder = new StringBuilder();
+            //    foreach (var item in order.Items)
+            //    {
+            //        builder.Append("{0}, {1}", item.BookId, item.Count);
+            //        builder.AppendLine();
+            //    }
 
-                message.Body = builder.ToString();
-                client.Send(message);
-            }
+            //    message.Body = builder.ToString();
+            //    client.Send(message);
+            //}
+
+            Debug.WriteLine("Order ID {0}", order.Id);
+            Debug.WriteLine("Delivery: {0}", (object)order.Delivery.Description);
+            Debug.WriteLine("Payment: {0}", (object)order.Payment.Description);
         }
     }
 }
